2ce2181f748967d0865117d93dd796b79cd20b95
JEANcarlo12345 <jeangonza0@gmail.com>
2021-01-26 17:29:45 -0500

    26.01.21_Jean_04

---
+++ Proyecto Realidad Aumentada/Assets/CANVAS/SCRIPTS/documento.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class documento : MonoBehaviour
+{
+    
+public void abrirDoc()
+    {
+        Application.OpenURL("https://drive.google.com/drive/folders/12yt-3GXeb-0DteBYwhSsvwydZw9pmmIO?usp=sharing");
+    }
+}
