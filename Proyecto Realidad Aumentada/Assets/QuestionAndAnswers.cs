74501edd331ec509bb024fe05ab5433785445e47
Pablituuu <keane.pa.si.in@gmail.com>
2021-01-31 17:34:20 -0500

    Revert "Revert "Pablito_31.01.21_2""
    This reverts commit b9c49b5fde538f2bf249f483b2ec227d768c6ba3.

---
+++ Proyecto Realidad Aumentada/Assets/QuestionAndAnswers.cs
@@ -0,0 +1,10 @@
+
+using UnityEngine;
+
+[System.Serializable]
+public class QuestionAndAnswers:MonoBehaviour
+{
+    public string Question;
+    public string[] Answers;
+    public int CorrectAnswer;
+}
