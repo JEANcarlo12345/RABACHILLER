d4e14d759c1fe444c91d97a2645605197051ec7d
Pablituuu <keane.pa.si.in@gmail.com>
2021-01-31 17:04:33 -0500

    Pablito_31.01.21_2

---
+++ Proyecto Realidad Aumentada/Assets/AnswerScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AnswerScript : MonoBehaviour
+{
+    public bool isCorrect = false;
+    public QuizManager quizManager;
+
+    public Color startColor;
+
+    private void Start()
+    {
+        startColor = GetComponent<Image>().color;
+    }
+
+    public void Answer()
+    {
+        if (isCorrect)
+        {
+            Debug.Log("Correct Answer");
+            quizManager.correct();
+        }
+        else
+        {
+            Debug.Log("Wrong Answer");
+            quizManager.wrong();
+        }
+    }
+
+}
