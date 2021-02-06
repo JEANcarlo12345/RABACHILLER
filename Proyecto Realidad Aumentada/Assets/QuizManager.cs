631eeac98b1c350af082bba5bcf118e683533c7a
JEANcarlo12345 <jeangonza0@gmail.com>
2021-02-06 15:22:58 -0500

    06-02.21_Jean_13

--- Proyecto Realidad Aumentada/Assets/QuizManager.cs
+++ Proyecto Realidad Aumentada/Assets/QuizManager.cs
@@ -28,14 +28,17 @@ public class QuizManager : MonoBehaviour
 
     public void retry()
     {
+ 
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        
     }
-
+   
     void GameOver()
     {
         Quizpanel.SetActive(false);
         GoPanel.SetActive(true);
         ScoreTxt.text = score + "/" + totalQuestions;
+
     }
 
     public void correct()
@@ -61,6 +64,7 @@ public class QuizManager : MonoBehaviour
 
     void SetAnswers()
     {
+       
         for (int i = 0; i < options.Length; i++)
         {
            
@@ -72,6 +76,7 @@ public class QuizManager : MonoBehaviour
                 options[i].GetComponent<AnswerScript>().isCorrect = true;
             }
         }
+        
     }
 
     void generateQuestion()
